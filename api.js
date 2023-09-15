cf60db6d528cd9aa08bb092fcd3ceaf828f059ef
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2023-09-15 14:08:11 +0700

    .

--- api.js
+++ api.js
@@ -2,13 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 
-// router.use(require("./flow/001/00LOGIN"))
-router.use(require("./flow/001/02MASTER_FINAL"))
-// router.use(require("./flow/003/flow003"))
-router.use(require("./flow/001/04MATCPlist"))
-router.use(require("./flow/001/05-01-INS_STD_MASTER"))
-router.use(require("./flow/001/05-02-INS_STD_IC"))
-router.use(require("./flow/001/05-04-INS_STD_FN"))
+router.use(require("./flow/001/01TEST"))
+
 
 // router.use(require("./flow/003/flow003"))
 // router.use(require("./flow/004/flow004"))
